b2df1706c56cb6ea56f23b40b3811717ddbe6532
Dmitry_Malyasov <dmalyasov24@gmail.com>
2022-09-10 14:38:00 +0800

    Task done

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿int AckFunction(int m, int n)
+{
+    if (m > 0 & n == 0)
+        return AckFunction(m - 1, 1);
+    if (m > 0 & n > 0)
+        return AckFunction(m-1, AckFunction(m, n - 1));
+    return n + 1;
+}
+
+int m = 2;
+int n = 3;
+
+Console.WriteLine("Функция Аккермана ({0},{1}) : {2}", m, n, AckFunction(m, n));
